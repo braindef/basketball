c73adf2539638d361ad926cecad5d0ccfb2644ad
Marc jun Landolt <mail@marclandolt.ch>
2017-08-22 23:52:07 +0200

    Edit

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ else $jahr=date(Y);
 			body{ padding: 40px; margin: auto; }
 			.current-row{background-color:#B24926;color:#FFF;}
 			.current-col{background-color:#5d501c;color:#FFF;}
-			.tbl-qa{width: 100%;font-size:0.9em;background-color: #f5f5f5; text-align:center;}
+			.tbl-qa{width: 100%;font-size:0.9em;background-color: #00f5f5; text-align:center;}
 			.tbl-qa th.table-header {padding: 5px;text-align: left;padding:10px;text-align:center;}
 			.tbl-qa .table-row td {padding:1px;background-color: #FDFDFD;}
                         .hidden { display: none; }
