713b2297a5ebfcad9dff9101d4b190fd9f03b290
Marc jun Landolt <mail@marclandolt.ch>
2017-08-22 23:04:54 +0200

    Edit

--- language-test.php
+++ language-test.php
@@ -12,3 +12,22 @@ echo strftime(" in German %A.\n");
 setlocale(LC_TIME, "de_DE.UTF-8");
 echo strftime(" in German %A.\n");
 ?>
+
+
+
+Code:
+setlocale(LC_TIME, "C");
+echo strftime("%A");
+setlocale(LC_TIME, "fi_FI");
+echo strftime(" in Finnish is %A,");
+setlocale(LC_TIME, "fr_FR");
+echo strftime(" in French %A and");
+setlocale(LC_TIME, "de_DE");
+echo strftime(" in German %A.\n");
+setlocale(LC_TIME, "de_DE.ISO8859-1");
+echo strftime(" in German %A.\n");
+setlocale(LC_TIME, "de_DE.UTF-8");
+echo strftime(" in German %A.\n");
+
+Hostpoint info Blatt:
+https://support.hostpoint.ch/de/01-Produkte/Webhosting/Allgemeine_Fragen_und_erste_Schritte/Wie_lauten_Datumsformat_und_Laendereinstellung_f%C3%BCr_PHP_locale
