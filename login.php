e9a22d9a00ef46172a9b314c35844a5c2d75b456
Marc jun Landolt <mail@marclandolt.ch>
2017-08-22 20:59:20 +0200

    Edit

--- login.php
+++ login.php
@@ -35,3 +35,27 @@ if(isset($_POST['login']))   // it checks whether the user clicked login button 
 <title> Login Page   </title>
 
 </head>
+
+<body>
+
+<form action="" method="post">
+
+    <table width="200" border="0">
+  <tr>
+    <td>  UserName</td>
+    <td> <input type="text" name="user" > </td>
+  </tr>
+  <tr>
+    <td> PassWord  </td>
+    <td><input type="password" name="pass"></td>
+  </tr>
+  <tr>
+    <td> <input type="submit" name="login" value="LOGIN"></td>
+    <td></td>
+  </tr>
+</table>
+</form>
+
+</body>
+</html>
+
