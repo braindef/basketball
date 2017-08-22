b2e98c6711c58d9c90f139806f2d54bdf5125f81
Marc jun Landolt <mail@marclandolt.ch>
2017-08-22 22:43:22 +0200

    Edit

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ else $jahr=date(Y);
     <head>
       <title>PHP MySQL Inline Editing using jQuery Ajax</title>
 		<style>
-			body{ margin:auto; }
+			body{ padding: 20px; margin: auto; }
 			.current-row{background-color:#B24926;color:#FFF;}
 			.current-col{background-color:#5d501c;color:#FFF;}
 			.tbl-qa{width: 100%;font-size:0.9em;background-color: #f5f5f5; text-align:center;}
