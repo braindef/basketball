56a7f8b9252ca5a4b7734102a6dcc3f5cb11ace1
Marc jun Landolt <mail@marclandolt.ch>
2017-08-22 22:12:02 +0200

    Edit

--- login.php
+++ login.php
@@ -39,7 +39,7 @@ if(isset($_POST['login']))   // it checks whether the user clicked login button 
 .container {
   width: 350px;
   height: 120px;
-  border-radius: 10px;
+  border-radius: 20px;
   background: #eee;
   margin: 50px auto;
   padding: 20px;
