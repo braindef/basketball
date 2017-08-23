63f3efadddd2b064fcaf92dacea8116b8ab80ffc
Marc jun Landolt <mail@marclandolt.ch>
2017-08-23 02:50:33 +0200

    Edit

--- index.php
+++ index.php
@@ -107,7 +107,7 @@ else
                                          if (strftime("%A", strtotime($faq[$k]["datum"]))=="Samstag") $frei=true;
                                          if (strftime("%A", strtotime($faq[$k]["datum"]))=="Sonntag") { $frei=true;
                                          echo $faq[$k]["datum"];
-                                         echo strftime("%A", strtotime($faq[$k]["datum"]); } ?> 
+                                         echo strftime("%A", strtotime($faq[$k]["datum"])); } ?> 
 
 				<td <?php if($frei) echo "style=\"background-color:#999;\""; //Samstage und Sonntage schwarz ?>
                                   <?php if ($faq[$k]["datum"]==date("Y-m-d")) echo "style=\"background-color:#0FF;\""; //aktuelles Datum Cyan farben ?> >
