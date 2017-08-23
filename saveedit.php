37e018fccf7356dc383035dc505f367c6155d954
Marc jun Landolt <mail@marclandolt.ch>
2017-08-23 03:34:01 +0200

    Edit

--- saveedit.php
+++ saveedit.php
@@ -1,7 +1,7 @@
 <?php
 require_once("dbcontroller.php");
 $db_handle = new DBController();
-$data = $_POST["editval"].trim()  //das html macht noch white-spaces die müssen weg
+$data = $_POST["editval"].trim();  //das html macht noch white-spaces die müssen weg
 
 $result = $db_handle->executeUpdate("UPDATE basketball set " . $_POST["column"] . " = '".$data."' WHERE  id=".$_POST["id"]);;
 ?>
