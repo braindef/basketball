f512471e8cc427d9a31860602ed3b9a1d8ba3a38
Marc jun Landolt <mail@marclandolt.ch>
2017-08-23 03:49:33 +0200

    Edit

--- saveedit.php
+++ saveedit.php
@@ -2,11 +2,13 @@
 require_once("dbcontroller.php");
 $db_handle = new DBController();
 $data = $_POST["editval"].trim();  //das html macht noch white-spaces die müssen weg
+$result = $db_handle->executeUpdate("UPDATE basketball set " . $_POST["column"] . " = '".$data."' WHERE  id=".$_POST["id"]);
+
 
 $date = new DateTime();
 $timestamp = $date->getTimestamp();
 $query = "UPDATE basketball set " . $_POST["column"] . " = '".$data."' WHERE  id=".$_POST["id"];
 $result = $db_handle->execute("INSERT into log VALUES (".$timestamp.",".$query.")";
 
-$result = $db_handle->executeUpdate("UPDATE basketball set " . $_POST["column"] . " = '".$data."' WHERE  id=".$_POST["id"]);
+
 ?>
