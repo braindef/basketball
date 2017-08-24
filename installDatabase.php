f7e61a2f9d2cb01cd385aa81dae98e1cd1b23778
Marc jun Landolt <mail@marclandolt.ch>
2017-08-24 14:54:15 +0200

    datenbank einrichten

---
+++ installDatabase.php
@@ -0,0 +1,12 @@
+<?php
+ session_start();
+ setlocale(LC_TIME, "de_DE.UTF-8"); //für deutsche Wochentage und Monate
+
+require_once("dbcontroller.php");
+$db_handle = new DBController();
+$sql = "SELECT * from basketball";
+$faq = $db_handle->runQuery($sql);
+
+<?php phpinfo(); ?>
+
+?>
