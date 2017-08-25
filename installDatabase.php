022089104f83618b36bd44a5ac6da0fc2a9c5281
Marc jun Landolt <mail@marclandolt.ch>
2017-08-25 22:53:40 +0200

    Edit

--- installDatabase.php
+++ installDatabase.php
@@ -2,21 +2,27 @@
  session_start();
  setlocale(LC_TIME, "de_DE.UTF-8"); //für deutsche Wochentage und Monate
 
-$mysql_host = "localhost";
-$mysql_database = "marcland_telli";
-$mysql_user = "root";
-$mysql_password = "123456";
+
+require_once("dbcontroller.php");
+$db_handle = new DBController();
+$db_handle->installDatabase();
+
+
+//$mysql_host = "localhost";
+//$mysql_database = "marcland_telli";
+//$mysql_user = "root";
+//$mysql_password = "123456";
 
 # MySQL with PDO_MYSQL  
-$db = new PDO("mysql:host=$mysql_host;dbname=$mysql_database", $mysql_user, $mysql_password);
-
-$query = file_get_contents("termine.sql");
-
-$stmt = $db->prepare($query);
-
-if ($stmt->execute())
-     echo "Success";
-else 
-     echo "Fail";
+//$db = new PDO("mysql:host=$mysql_host;dbname=$mysql_database", $mysql_user, $mysql_password);
+//
+//$query = file_get_contents("termine.sql");
+//
+//$stmt = $db->prepare($query);
+//
+//if ($stmt->execute())
+//     echo "Success";
+//else 
+//     echo "Fail";
 
 ?>
