bd64b57e1e73cc364c5ea51ac15586cad9ab2d6c
Marc jun Landolt <mail@marclandolt.ch>
2017-08-25 21:27:53 +0200

    Edit

--- saveedit.php
+++ saveedit.php
@@ -4,6 +4,11 @@ $db_handle = new DBController();
 $data = trim($_POST["editval"]);  //das html macht noch white-spaces die müssen weg
 $result = $db_handle->executeUpdate("UPDATE basketball set " . $_POST["column"] . " = '".$data."' WHERE  id=".$_POST["id"]);
 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
 
 $date = new DateTime();
 $timestamp = $date->getTimestamp();
@@ -11,3 +16,17 @@ $result = $db_handle->executeUpdate("INSERT into log VALUES (".$timestamp.",".$q
 if ($result) echo "log success";
 else echo "log fail";
 ?>
+
+
+
+temp
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
