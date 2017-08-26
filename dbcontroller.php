48d760834168c81605c6ef8d7e61cd4791794644
Marc jun Landolt <mail@marclandolt.ch>
2017-08-26 04:46:56 +0200

    Edit

--- dbcontroller.php
+++ dbcontroller.php
@@ -49,7 +49,7 @@ class DBController {
 //	}
 
 	function connectDB() {
-		$db = new PDO('mysql:host='.$this->host.';dbname='.$this->$database.';charset=utf8mb4',$this->user, $this->password);
+		$db = new PDO('mysql:host='.$this->host.';dbname='.$this->database.';charset=utf8mb4',$this->user, $this->password);
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 		return $db;
