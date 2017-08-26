343da6cf479dc36d76d1dbf519bfa6bbd3854228
Marc jun Landolt <mail@marclandolt.ch>
2017-08-26 06:05:34 +0200

    Edit

--- saveedit.php
+++ saveedit.php
@@ -9,10 +9,9 @@ $result = $db_handle->executeUpdate($query);
 $date = new DateTime();
 $timestamp = $date->getTimestamp();
 
-$log = "INSERT INTO log (timestamp,ip, proxy, query)
-VALUES (\"".$timestamp."\",\"".$_SERVER['REMOTE_ADDR']."\",\"".$_SERVER['HTTP_X_FORWARDED_FOR']."\",\"".$query."\")";
+$log = "INSERT INTO log (timestamp,ip, proxy, query) VALUES (\"".$timestamp."\",\"".$_SERVER['REMOTE_ADDR']."\",\"".$_SERVER['HTTP_X_FORWARDED_FOR']."\",\"".$query."\")";
 
-if ($db_handle->db->query($log) === TRUE) {
+if ($db_handle->query($log) === TRUE) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $log . "<br>" . $conn->error;
