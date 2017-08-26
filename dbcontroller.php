e97c9f00241ce2d2b1d014d1064d91c53361a20c
Marc jun Landolt <mail@marclandolt.ch>
2017-08-26 05:32:44 +0200

    Edit

--- dbcontroller.php
+++ dbcontroller.php
@@ -87,7 +87,7 @@ class DBController {
 	function runQuery($query) {
 		foreach($this->db->query($query) as $row) {
 			array_push($resultset,$row)		
-			print_r($resultset);
+			//print_r($resultset);
 		}
 		if(!empty($resultset))
 		return $resultset;
