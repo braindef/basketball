fe656ec9b3429038743ef38b5f48791663659ed1
Marc jun Landolt <mail@marclandolt.ch>
2017-08-26 04:42:30 +0200

    Edit

--- dbcontroller.php
+++ dbcontroller.php
@@ -1,4 +1,21 @@
 <?php
+//if $_GET["install"]==1
+
+if ($_GET["test"]==1)
+{
+	echo "Installing Database";
+	$db_handle = new DBController();
+	$db_handle->installDatabase();
+}
+
+
+if ($_GET["test"]==1)
+{
+	echo "Testing Database";
+	$db_handle = new DBController();
+	$db_handle->test();
+}
+
 class DBController {
 	private $host = "marcland.mysql.db.internal";
 	//private $host = "localhost"; 
@@ -11,25 +28,50 @@ class DBController {
 		$this->conn = $this->connectDB();
 	}
 	
+//	function connectDB() {
+//		$conn = mysqli_connect($this->host,$this->user,$this->password,$this->database);
+//                if($conn==false)
+//                echo "<font color=red>Database Connection Error</font>";
+//                else
+//		{
+//                echo "<!--Connected to database-->";
+//		if ($result = mysqli_query($this->conn,"SHOW TABLES LIKE 'basketball'")) {
+//		    if($result->num_rows == 1) {
+//			return;
+//		    }
+//		}
+//		else {
+//			echo "Table does not exist";
+//			$this->installDatabase();
+//		}
+//		}
+//		return $conn;
+//	}
+
 	function connectDB() {
-		$conn = mysqli_connect($this->host,$this->user,$this->password,$this->database);
-                if($conn==false)
-                echo "<font color=red>Database Connection Error</font>";
-                else
-		{
-                echo "<!--Connected to database-->";
-		if ($result = mysqli_query($this->conn,"SHOW TABLES LIKE 'basketball'")) {
-		    if($result->num_rows == 1) {
-			return;
-		    }
+		$db = new PDO('mysql:host='.$this->host.';dbname='.$this->$database.';charset=utf8mb4',$this->user, $this->password);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+		return $db;
+}
+
+
+
+	function test() {
+		try {
+			//connect as appropriate as above
+			$db->query('hi'); //invalid query!
+		} catch(PDOException $ex) {
+			echo "An Error occured!"; //user friendly message
+			//some_logging_function($ex->getMessage());
 		}
-		else {
-			echo "Table does not exist";
-			$this->installDatabase();
+
+		foreach($db->query('SELECT * FROM test') as $row) {
+			    echo "Morgen: ". $row['morgen'].' abends: '.$row['abends']."<br>";
 		}
-		}
-		return $conn;
-	}
+}
+
+
 	
 	function runQuery($query) {
 		$result = mysqli_query($this->conn,$query);
