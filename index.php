37dcc17f3db8745ebca75337e57401c124f923b5
Marc Landolt <mail@marclandolt.ch>
2017-08-30 16:59:35 +0200

    text

--- index.php
+++ index.php
@@ -11,13 +11,13 @@
           <meta charset="utf-8">
     </head>
     <body>
-<div style="float:left; margin: 10px; width:40%;> <a title="Home" href="https://gztelli.ch"><img src="./gztelli.svg" alt="GZ Telli" width=300> </a> </div><div class="startseite" style="float:left;"><a class="button" href="agenda.php">Kalender anschauen</a></div><div class="startseite" style="float:none;"> <a class="button" href="login.php">Kalender editieren</a></div>
+<div style="float:left; margin: 10px; width:40%;> <a title="Home" href="https://gztelli.ch"><img src="./gztelli.svg" alt="GZ Telli" width=300> </a> </div><div class="startseite" style="float:left;"><a class="button" href="agenda.php">Kalender anschauen</a></div><div class="startseite" style="float:none;"> <a class="button" href="login.php">Kalendereintrag machen</a></div>
 <br>
 <br>
 <br>
 <pre>
 <font size=5em>
-Am 17.August 2017 fand ein <b>Runder Tisch</b> statt im Gemeinschaftszentrums Telli.
+Am 17.August 2017 fand ein <b>Runder Tisch</b> statt im Gemeinschaftszentrum Telli.
 
 <b>Ziel</b> war es, eine sowohl für die AnwohnerInnen des Aaredörfli als auch für die Jugendlichen 
 taugliche Lösung des Nutzungskonflikts (Lärmproblematik) rund um das Basketballspielen auf dem 
@@ -35,24 +35,30 @@ Folgende <b>Parteien</b> nahmen am Runden Tisch teil:
 
 Folgender Kompromiss wurde von den Anwesenden ausgehandelt und vereinbart:
 
-Die Baskettballkörbe werden jeden Tag in der Saison (Frühlingsferien - Mitte November) um 18.00 
+Die Basketballkörbe werden während Saison (Frühlingsferien - Mitte November) um 18.00 Uhr
 von <b>Freiwilligen</b> abgehängt und zusammen mit der Stange für das Auf- bzw. Abhängen, an einem 
 abschliessbaren Haken deponiert.  
 
-Der Hauswart hängt die Körbe am Morgen wieder auf, ausser während den Schulferien. Dann übernehmen 
+Der Hauswart hängt die Körbe am Morgen wieder auf, ausser während der Schulferien. Dann übernehmen 
 Freiwillige diese Arbeit.
 
 Das GZ Telli ist dafür verantwortlich, dass die Körbe um 18.00 Uhr abgehängt werden.
 
 <b>Wenn Sie sich einen Einsatz am Abend vorstellen können</b>, schauen Sie sich die Datenbank an 
-(KNOPF machen, der auf den aktuellen Monat führt). An den farbig hinterlegten Tagen suchen wir 
+(KNOPF oben, Kalender anschauen). An den farbig hinterlegten Tagen suchen wir 
 noch Freiwillige, die die Körbe ab – bzw. aufhängen würden.
 
 <b>Wenn Sie sich für einen Einsatz entschieden haben</b>, melden Sie sich per Mail oder Telefon im 
-GZ. Wir geben Ihnen dann das Passwort bekannt und regeln die Schlüsselübergabe
+Gemeinschaftszentrum Telli. Wir geben Ihnen dann das Passwort bekannt, mit dem Sie oben rechts auf den Knopf „Kalendereintrag machen“ drücken können. Die Schlüsselübergabe wird ebenfalls mit dem Gemeinschaftszentrum geregelt.
 
-Login für die Eingabe mit Passwort zum Eintragen machen
 
+Kontakt:
+Gemeinschaftszentrum Telli
+062 824 63 44
+info@gztelli.ch
+
+
+e
 Bilder der Aufhängung:
 </font><pre>
 <a href="./basketball0.jpg" ><img height=200 src="./basketball0.jpg"/></a> <a href="./basketball1.jpg" ><img height=200 src="./basketball1.jpg"/></a>
