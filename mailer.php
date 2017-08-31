20f26b84ecbb19961e1a52beb0a52815d48039f0
marcland <marcland@s30.server.hostpoint.ch>
2017-09-01 00:44:33 +0200

    automailer

--- mailer.php
+++ mailer.php
@@ -6,16 +6,16 @@ setlocale(LC_TIME, "de_CH.utf8"); //fÃ¼r deutsche Wochentage und Monat
 require_once("dbcontroller.php");
 
 echo "<pre>";
-echo "Working Directory:";
-echo getcwd() . "\n";
+echo "Working Directory: ";
+echo getcwd() . "/mailer.php \n";
 echo "";
 echo "</pre>";
 
 $db_handle = new DBController();
 
+//morgiges datum rausfinden
 $datetime = new DateTime('tomorrow');
 $tomorrow = $datetime->format('Y-m-d');
-//if ($datetime->format(''
 
 
 $to = "mail@marclandolt.ch";
@@ -23,11 +23,14 @@ $subject = "Morgen hat es keinen Eintrag im Basketball Kalender";
 $txt = "http://basketball.gztelli.ch/agenda.php";
 $headers = "From: basketball@gztelli.ch";
 
+//am wochenende ist frei
 $frei = false;
+
+//senden wenn entweder morgen oder abend nicht eingetragen ist.
 $send = false;
 
 $sql = "SELECT * from basketball where datum=\"".$tomorrow."\"";
-//echo $sql;
+echo $sql;
 $faq = $db_handle->runQuery($sql);
 
 foreach($faq as $k=>$v)
@@ -35,12 +38,14 @@ foreach($faq as $k=>$v)
   if (strftime("%A", strtotime($faq[$k]["datum"]))==strftime("%A", strtotime("2017-08-26"))) $frei=true;
   if (strftime("%A", strtotime($faq[$k]["datum"]))==strftime("%A", strtotime("2017-08-27"))) $frei=true;  
 
+  //wenn morgen leer dann senden setzen
   if ($faq[$k]["morgen"]=="")
   {
      echo "Morgen fehlt ";
      $send = true;
   }     
 
+  //wenn abends leer dann senden setzen
   if ($faq[$k]["abends"]=="")
   {  echo "Abend fehlt ";
      $send = true;
